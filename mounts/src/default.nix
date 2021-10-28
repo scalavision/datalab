2442138a5c9d5c5671ba02e154056ba9e8bb01fb
scalavision <scalavision@gmail.com>
2021-10-28 20:13:28 +0200

    update nix-shell default env

--- mounts/src/default.nix
+++ mounts/src/default.nix
@@ -1,28 +1,27 @@
-let
-  pkgs = import <nixpkgs> {};
-in
-  pkgs.mkShell {
-    name = "simpleEnv";
-    buildInputs = with pkgs; [
+let pkgs = import <nixpkgs> { };
+in pkgs.mkShell {
+  name = "simpleEnv";
+  buildInputs = with pkgs; [
     # basic python dependencies
-      python38
-      python38Packages.numpy
-      python38Packages.scikitlearn
-      python38Packages.scipy
-      python38Packages.matplotlib
+    python38
+    python38Packages.numpy
+    python38Packages.scikitlearn
+    python38Packages.scipy
+    python38Packages.matplotlib
     # a couple of deep learning libraries
-      python38Packages.tensorflow
-      python38Packages.Keras
-      python38Packages.pytorch
-
+    python38Packages.tensorflow
+    python38Packages.Keras
+    python38Packages.pytorch
+    openjdk11
     # Lets assume we also want to use R, maybe to compare sklearn and R models
-      R
-      rPackages.mlr
-      rPackages.data_table # "_" replaces "."
-      rPackages.ggplot2
-    ];
-    doCheck = false;
-   shellHook = ''
-      '';
-  }
-  
+    R
+    rPackages.mlr
+    rPackages.data_table # "_" replaces "."
+    rPackages.ggplot2
+  ];
+  doCheck = false;
+  shellHook = ''
+    export JAVA_HOME=${openjdk11}/lib/openjdk
+    export COURSIER_CACHE=/yoda/.cache
+  '';
+}
