2c2767241b9ae577597e4580c06944fc3c5f1868
scalavision <scalavision@gmail.com>
2021-11-04 21:04:23 +0100

    [devops] rename nix build scripts

---
+++ .devcontainer/build_from_alpine.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+VERSION=${1:-2.4}
+IMAGE_NAME=${2:-nix}
+IMAGE_TAG=${3:-$VERSION}
+
+echo "building docker image ${IMAGE_NAME}}:${IMAGE_TAG} with nix version: ${VERSION}"
+
+cat bootstrap.sh "${VERSION}" | docker run --network=host --rm -i alpine sh > nix.tar.gz
+docker build --rm --network host -t "${IMAGE_NAME}:${IMAGE_TAG}" .
